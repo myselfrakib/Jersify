d630711a790331f49c2e465ad196262259dafa43
Your Name <you@example.com>
2026-06-14 17:57:24 +0530

    chore: remove temporary scripts

--- find_logos.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const files = fs.readdirSync('.').filter(f => f.endsWith('.html'));
-files.forEach(f => {
-  const content = fs.readFileSync(f, 'utf8');
-  const match = content.match(/<[^>]+class=["']logo["'][^>]*>.*?<\/[^>]+>/s);
-  if (match) {
-    console.log(f + ': ' + match[0].trim());
-  }
-});
